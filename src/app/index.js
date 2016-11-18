fa9b2a2f66fe54eade27b646245be9c075676e75
jttwnsnd <jt.a.townsend@gmail.com>
2016-11-18 16:08:47 -0500

    allows correct rendering

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,17 @@
 console.log("it works")
+
+import React from "react";
+import { render } from "react-dom";
+
+import { Home } from "./components/Home";
+
+class App extends React.Component {
+  render() {
+		return (
+			<div className="container">
+        <Home/>
+			</div>
+		);
+	}
+}
+render(<App/>, window.document.getElementById("app"));
