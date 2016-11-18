8b2b1ff65bce2cf9d99c93ddc5d21cb9b3195f76
jttwnsnd <jt.a.townsend@gmail.com>
2016-11-18 14:05:07 -0500

    Added components folder

---
+++ src/app/components/Home-min.js
@@ -0,0 +1,1 @@
+"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}var _react=require("react"),_react2=_interopRequireDefault(_react);
---
+++ src/app/components/Home.js
@@ -0,0 +1,14 @@
+/*
+This is a sample file for your components folder. Construct your components in here.
+*/
+import React from "react";
+
+export class extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    )
+  }
+}
